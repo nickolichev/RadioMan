2ef2c37893ab60dad5dcdeb7a153231583b48209
nickolichev@gmail.com <nickolichev@gmail.com>
2022-06-08 22:59:10 +0300

    one

---
+++ src/main/java/ru/netology/radiostation/RadioSettings.java
@@ -0,0 +1,103 @@
+package ru.netology.radiostation;
+
+public class RadioSettings {
+
+    private int currentStation;
+
+    public void setNextStation(int[] stations) {
+
+        int currentStation;
+        int lastStation = 9;
+
+        for (currentStation = 0; currentStation < stations.length; currentStation++) {
+
+            currentStation = currentStation + 1;
+
+            if (currentStation > 8) {
+
+                lastStation = 0;
+            }
+            this.currentStation = lastStation;
+        }
+    }
+
+    public int getNextStation() {
+
+        return this.currentStation;
+    }
+
+    public void setPrevStation(int[] stations) {
+
+        int currentStation;
+        int lastStation = 0;
+
+        for (currentStation = 0; currentStation < stations.length; currentStation++) {
+
+            if (currentStation == 0) {
+
+                lastStation = 9;
+            }
+            this.currentStation = lastStation;
+        }
+    }
+
+    public int getPrevStation() {
+
+        return currentStation;
+    }
+
+    public void setNumStation(int station) {
+
+        if (station > 0) {
+
+            if (station < 10) {
+
+                currentStation = station;
+            }
+        }
+    }
+
+    public int getNumStation() {
+
+        return currentStation;
+    }
+
+    private int currentVolume;
+
+    public void setIncreaseVolume(int levelVolume) {
+
+        if (levelVolume < 10) {
+
+            currentVolume = levelVolume + 1;
+        }
+
+        if (levelVolume == 10) {
+
+            currentVolume = levelVolume;
+        }
+    }
+
+    public int getIncreaseVolume() {
+
+        return currentVolume;
+    }
+
+    public void setDecreaseVolume(int levelVolume) {
+
+        if (levelVolume < 10) {
+
+            currentVolume = levelVolume - 1;
+        }
+
+        if (levelVolume == 0) {
+
+            currentVolume = levelVolume;
+        }
+    }
+
+    public int getDecreaseVolume() {
+
+        return currentVolume;
+    }
+
+}
---
+++ src/test/java/ru/netology/radiostation/RadioTests.java
@@ -0,0 +1,125 @@
+package ru.netology.radiostation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTests {
+
+    @Test
+    void stationNext() {
+
+        RadioSettings station = new RadioSettings();
+
+        int[] stations = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        station.setNextStation(stations);
+
+        int actual = station.getNextStation();
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void stationPrev() {
+
+        RadioSettings station = new RadioSettings();
+
+        int[] stations = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+        station.setPrevStation(stations);
+
+        int actual = station.getPrevStation();
+        int expected = 9;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void stationNum() {
+
+        RadioSettings station = new RadioSettings();
+
+        station.setNumStation(5);
+
+        int actual = station.getNumStation();
+        int expected = 5;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void IncreaseVolume() {
+
+        RadioSettings station = new RadioSettings();
+
+        int levelVolume = 5;
+
+        station.setIncreaseVolume(levelVolume);
+
+        int actual = station.getIncreaseVolume();
+        int expected = 6;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void MaxIncreaseVolume() {
+
+        RadioSettings station = new RadioSettings();
+
+        int levelVolume = 10;
+
+        station.setIncreaseVolume(levelVolume);
+
+        int actual = station.getIncreaseVolume();
+        int expected = 10;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void DecreaseVolume() {
+
+        RadioSettings station = new RadioSettings();
+
+        int levelVolume = 6;
+
+        station.setDecreaseVolume(levelVolume);
+
+        int actual = station.getDecreaseVolume();
+        int expected = 5;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void MinDecreaseVolume() {
+
+        RadioSettings station = new RadioSettings();
+
+        int levelVolume = 0;
+
+        station.setDecreaseVolume(levelVolume);
+
+        int actual = station.getDecreaseVolume();
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
