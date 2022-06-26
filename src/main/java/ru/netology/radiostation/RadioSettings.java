16a9711e315900995041b28db3ce57d6d7bbb683
nickolichev@gmail.com <nickolichev@gmail.com>
2022-06-26 14:59:22 +0300

    revision_one

--- src/main/java/ru/netology/radiostation/RadioSettings.java
+++ src/main/java/ru/netology/radiostation/RadioSettings.java
@@ -2,77 +2,45 @@ package ru.netology.radiostation;
 
 public class RadioSettings {
 
-    private Boolean nextStation = true;
-    private Boolean prevStation = true;
-    private Boolean nextVolume = true;
-    private Boolean prevVolume = true;
-    private int rangeDefault = 10;
+    private int rangeDefault;
     private int range;
-    private int minStation = 0;
-    private int maxStation = 9;
     private int currentStation;
-    private int stepStation = 1;
-    private int minVolume = 0;
-    private int maxVolume = 100;
     private int currentVolume;
-    private int stepVolume = 1;
 
-    public RadioSettings(
-            Boolean nextStation,
-            Boolean prevStation,
-            Boolean nextVolume,
-            Boolean prevVolume,
-            int rangeDefault,
-            int minStation,
-            int maxStation,
-            int stepStation,
-            int minVolume,
-            int maxVolume,
-            int stepVolume) {
+    public RadioSettings(int range) {
+        this.range = range;}
 
-        this.nextStation = nextStation;
-        this.prevStation = prevStation;
-        this.nextVolume = nextVolume;
-        this.prevVolume = prevVolume;
-        this.rangeDefault = rangeDefault;
-        this.minStation = minStation;
-        this.maxStation = maxStation;
-        this.stepStation = stepStation;
-        this.minVolume = minVolume;
-        this.maxVolume = maxVolume;
-        this.stepVolume = stepVolume;
-    }
-    public RadioSettings() {
-        this.range = 10;
-    }
+    public RadioSettings() {}
+
     public void setCurrentStation(int currentStation) {
 
-        if (currentStation > maxStation) {
+        if (currentStation > 9) {
             this.currentStation = 100;
             return;
         }
 
-        if (currentStation < minStation) {
+        if (currentStation < 0) {
             this.currentStation = -100;
             return;
         }
 
-        if (currentStation == maxStation) {
-            this.currentStation = minStation;
+        if (currentStation == 9) {
+            this.currentStation = 0;
             return;
         }
 
-        if (currentStation == minStation) {
-            this.currentStation = maxStation;
+        if (currentStation == 0) {
+            this.currentStation = 9;
         }
     }
     public int getCurrentStation() {
         return currentStation;
     }
+
     public void setCurrentNext(Boolean nextStation, int currentStation) {
 
         if (nextStation) {
-            this.currentStation = currentStation + stepStation;
+            this.currentStation = currentStation + 1;
         } else {
             this.currentStation = currentStation;
         }
@@ -80,10 +48,11 @@ public class RadioSettings {
     public int getCurrentNext() {
         return this.currentStation;
     }
+
     public void setCurrentPrev(Boolean prevStation, int currentStation) {
 
         if (prevStation) {
-            this.currentStation = currentStation - stepStation;
+            this.currentStation = currentStation - 1;
         } else {
             this.currentStation = currentStation;
         }
@@ -91,10 +60,11 @@ public class RadioSettings {
     public int getCurrentPrev() {
         return this.currentStation;
     }
+
     public void setCurrentDefault(int rangeDefault) {
 
         if (0 <= rangeDefault && rangeDefault <= 10) {
-            this.rangeDefault = rangeDefault;
+            this.rangeDefault = 10;
         } else {
             this.rangeDefault = 100;
         }
@@ -102,34 +72,40 @@ public class RadioSettings {
     public int getCurrentDefault() {
         return rangeDefault;
     }
+
     public void setCurrentRange(int range) {
-        this.range = range;
+        if (range == 5) {
+            this.rangeDefault = 5;
+        } else {
+            this.rangeDefault = 0;
+        }
     }
     public int getCurrentRange() {
-        return range;
+        return rangeDefault;
     }
+
     public void setCurrentVolume(int currentVolume) {
 
-        if (currentVolume < minVolume) {
-            this.currentVolume = minVolume;
+        if (currentVolume < 0) {
+            this.currentVolume = 0;
         }
 
-        if (currentVolume > maxVolume) {
-            this.currentVolume = maxVolume;
+        if (currentVolume > 100) {
+            this.currentVolume = 100;
         }
     }
     public int getCurrentVolume() {
         return currentVolume;
     }
+
     public void setVolumeNext(Boolean nextVolume, int currentVolume) {
 
         if (nextVolume) {
-            this.currentVolume = currentVolume + stepVolume;
+            this.currentVolume = currentVolume + 1;
         } else {
             this.currentVolume = currentVolume;
         }
     }
-
     public int getVolumeNext() {
         return this.currentVolume;
     }
@@ -137,7 +113,7 @@ public class RadioSettings {
     public void setVolumePrev(Boolean prevStation, int currentVolume) {
 
         if (prevStation) {
-            this.currentVolume = currentVolume - stepVolume;
+            this.currentVolume = currentVolume - 1;
         } else {
             this.currentVolume = currentVolume;
         }
--- src/test/java/ru/netology/radiostation/RadioTests.java
+++ src/test/java/ru/netology/radiostation/RadioTests.java
@@ -1,133 +1,147 @@
 package ru.netology.radiostation;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTests {
-    RadioSettings station = new RadioSettings(
-            true,
-            true,
-            true,
-            true,
-            10,
-            0,
-            9,
-            1,
-            0,
-            100,
-            1) {
+    RadioSettings station = new RadioSettings() {
     };
-    RadioSettings range = new RadioSettings() {
-    };
+
+    RadioSettings range = new RadioSettings(5) {};
+
     @Test
     void settingNext() {
 
         station.setCurrentNext(true, 6);
         assertEquals(7, station.getCurrentNext());
     }
+
     @Test
     void settingNextFalse() {
 
         station.setCurrentNext(false, 6);
         assertEquals(6, station.getCurrentNext());
     }
+
     @Test
     void settingPrev() {
 
         station.setCurrentPrev(true, 4);
         assertEquals(3, station.getCurrentPrev());
     }
+
     @Test
     void settingPrevFalse() {
 
         station.setCurrentPrev(false, 4);
         assertEquals(4, station.getCurrentPrev());
     }
+
     @Test
     void settingsDefault() {
 
         station.setCurrentDefault(10);
         assertEquals(10, station.getCurrentDefault());
     }
+
     @Test
     void settingsDefaultUp() {
 
         station.setCurrentDefault(11);
         assertEquals(100, station.getCurrentDefault());
     }
+
     @Test
     void settingsDefaultDown() {
 
         station.setCurrentDefault(-1);
         assertEquals(100, station.getCurrentDefault());
     }
+
     @Test
     void settingsUp() {
 
         station.setCurrentStation(11);
         assertEquals(100, station.getCurrentStation());
     }
+
     @Test
     void settingsDown() {
 
         station.setCurrentStation(-1);
         assertEquals(-100, station.getCurrentStation());
     }
+
     @Test
     void settingsMax() {
 
         station.setCurrentStation(9);
         assertEquals(0, station.getCurrentStation());
     }
+
     @Test
     void settingsMin() {
 
         station.setCurrentStation(0);
         assertEquals(9, station.getCurrentStation());
     }
+
     @Test
     void settingsMinFalse() {
 
         station.setCurrentStation(1);
         assertEquals(0, station.getCurrentStation());
     }
+
     @Test
     void settingsRange() {
 
-        station.setCurrentRange(10);
-        assertEquals(10, station.getCurrentRange());
+        range.setCurrentRange(5);
+        assertEquals(5, range.getCurrentRange());
     }
+
+    @Test
+    void settingsRangeFalse() {
+
+        range.setCurrentRange(9);
+        assertEquals(0, range.getCurrentRange());
+    }
+
     @Test
     void settingsVolumeNext() {
 
         station.setVolumeNext(true, 50);
         assertEquals(51, station.getVolumeNext());
     }
+
     @Test
     void settingsVolumeNextFalse() {
 
         station.setVolumeNext(false, 50);
         assertEquals(50, station.getVolumeNext());
     }
+
     @Test
     void settingsVolumePrev() {
 
         station.setVolumePrev(true, 50);
         assertEquals(49, station.getVolumePrev());
     }
+
     @Test
     void settingsVolumePrevFalse() {
 
         station.setVolumePrev(false, 40);
         assertEquals(40, station.getVolumePrev());
     }
+
     @Test
     void settingsVolumeUp() {
 
         station.setCurrentVolume(101);
         assertEquals(100, station.getCurrentVolume());
     }
+
     @Test
     void settingsVolumeDown() {
 
